79298acd8c72b2a14e496095f50d16ecd09ca031
BrianTirafiA <114814776+BrianTirafiA@users.noreply.github.com>
2024-02-03 18:34:06 +0700

    firestore dikit

--- app/src/main/java/com/example/bootcamp/login.kt
+++ app/src/main/java/com/example/bootcamp/login.kt
@@ -2,10 +2,24 @@ package com.example.bootcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class login : AppCompatActivity() {
+    private lateinit var username : EditText
+    private lateinit var password : EditText
+    private lateinit var btnlogin : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        username = findViewById(R.id.username_input)
+        password = findViewById(R.id.pass_input)
+        btnlogin = findViewById(R.id.login_btn)
+
+        btnlogin.setOnClickListener{
+            
+        }
     }
 }
