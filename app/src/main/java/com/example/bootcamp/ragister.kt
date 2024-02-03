fa3080548d5b0c1cf7ae4b3a78a887d961b661aa
BrianTirafiA <114814776+BrianTirafiA@users.noreply.github.com>
2024-02-03 13:33:58 +0700

    new layout

---
+++ app/src/main/java/com/example/bootcamp/ragister.kt
@@ -0,0 +1,11 @@
+package com.example.bootcamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ragister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ragister)
+    }
+}
