ecca214ed1b251bbe7efab1eb862e6a76f3fd5e1
JSometwo <mhafidzalfarisi543@gmail.com>
2024-02-05 10:01:20 +0700

    postforum

--- app/src/main/java/com/example/bootcamp/account.kt
+++ app/src/main/java/com/example/bootcamp/account.kt
@@ -3,7 +3,7 @@ package com.example.bootcamp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class account : AppCompatActivity() {
+class  account : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
